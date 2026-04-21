4d3fad31726c1984b4c5ab7b6cf07947701f2bdd
KozmikZ <zbynek.kozmik.jr@gmail.com>
2026-04-21 16:27:11 +0200

    plane velocity

--- draclove-love/src/game.lua
+++ draclove-love/src/game.lua
@@ -2,24 +2,51 @@ local L = require("lib/l")
 
 function L.setup()
 	L.plane = {x=0, y=0, dead=false, velocity=0}
+end
+
+local function change_plane_velocity(dt)
+	local downspeed_const = 300
+	local upspeed_const = -300
+	local down_velocity_min = 1000
+	local cap_const_up = 10
+	local cap_const_down = -50
+	if L.plane.velocity < down_velocity_min then
+		if L.plane.velocity > cap_const_down and L.plane.velocity < cap_const_up then
+			L.plane.velocity = cap_const_up
+		else
+			L.plane.velocity = L.plane.velocity + dt * downspeed_const 
+		end
+		L.print(""..L.plane.velocity)	
+	end
+	if L.key_pressed("space")  then
+		L.plane.velocity = upspeed_const
+	end
+
+
+	
 
 end
 
 local function move_plane(dt)
-	local speed_const = 1
-	local upspeed_const = 10
-	local down_velocity_min = -10
-	if L.plane.velocity > down_velocity_min then
-		L.plane.velocity = L.plane.velocity - dt * speed_const 
-	end
-	if L.key_released("space")  then
-		L.plane.velocity = upspeed_const
+	local updated_coords = L.plane.y + L.plane.velocity * dt
+
+	if updated_coords >= -L.height/2 and updated_coords <= L.height/2 then
+		L.plane.y = updated_coords
+	elseif updated_coords <= -L.height/2 then
+		L.plane.y=-L.height/2
+	else
+		L.plane.y=L.height/2
 	end
 end
 
 function L.render(dt)
+	change_plane_velocity(dt)
 	move_plane(dt)
 	L.draw({text="Dt: " .. dt, font="pixelifysans",font_size=42,align="mm",x=0,y=0})
 	L.draw(L.plane)
+	if L.key_released("backspace") then 
+		L.reset()
+	end
+
 end
 
