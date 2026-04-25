f8bf87b666cc6700dfa89a84bf42878775b027d8
KozmikZ <zbynek.kozmik.jr@gmail.com>
2026-04-25 12:51:48 +0200

    merge - fixed movement

--- draclove-love/src/game.lua
+++ draclove-love/src/game.lua
@@ -3,14 +3,15 @@ local LM = require("src/levelManager")
 L.clear_pck_cache()
 local L1 = require("src/level1")
 -- as of now, all levels MUST have a .setup function and a .loop function
-L.levels = {L1}
+L.levels = { L1 }
 L.hunger_limit = 3
+
 
 function L.setup()
 	L1.setup()
 	
 	-- dialogue event = {text="text", audio="path_to_my_audio_file"}
-	L.dialogue_manager = {events = {que={}, next_pop_i = 1, next_add_i = 1}, next_dialogue_at = nil}
+	L.dialogue_manager = { events = { que = {}, next_pop_i = 1, next_add_i = 1 }, next_dialogue_at = nil }
 	function L.player.take_damage()
 		if L.player.is_dodging() then
 			return false
@@ -29,16 +30,13 @@ function L.setup()
 
 	-- set the level variable for this file
 	L.active_level = L1
-	L.push_dialogue({text="Hello idiot, how are you doing??",audio=nil})
+	L.push_dialogue({ text = "Hello idiot, how are you doing??", audio = nil })
 end
-
-
 
 function L.push_dialogue(dialogue_event)
 	local add = L.dialogue_manager.events.next_add_i
-	L.dialogue_manager.events.que[add] = dialogue_event 
+	L.dialogue_manager.events.que[add] = dialogue_event
 	L.dialogue_manager.events.next_add_i = L.dialogue_manager.events.next_add_i + 1
-
 end
 
 function L.pop_dialogue()
@@ -54,52 +52,81 @@ function L.play_dialogue()
 		local dialogue = L.pop_dialogue()
 		if dialogue ~= nil then
 			-- L.play(dialogue.audio)
-			table.insert(L.active_level.level.np_objects,{x = -5 * string.len(dialogue.text) ,y=310,is_dead_at = L.time() + string.len(dialogue.text) * 0.05, text=dialogue.text ,tag = "text", font="pixelifysans", font_size = 30})
+			table.insert(L.active_level.level.np_objects,
+				{
+					x = -5 * string.len(dialogue.text),
+					y = 310,
+					is_dead_at = L.time() + string.len(dialogue.text) * 0.05,
+					text =
+						dialogue.text,
+					tag = "text",
+					font = "pixelifysans",
+					font_size = 30
+				})
 		end
 	end
-	
 end
 
+local function player_anime(sprite, t)
+	L.player.sprite = sprite
+	L.player.sprite_t = t
+	L.player.sprite_start = L.time()
+end
 
 local movement_const = 60
 local dodge_cooldown = 0.5
 local function player_action()
 	if L.key_pressed("c") and L.time() - L.player.last_dodged > dodge_cooldown then
-		L.player.sprite = "player/matrix"
-		L.player.sprite_t = 0.05
-		L.player.sprite_start = L.time()
+		player_anime("player/matrix", 0.06)
 		L.player.pr, L.player.pl = 0, 0
 		L.player.pt = -30
 		L.player.last_dodged = L.time()
 	end
 	if L.key_pressed("x") then
-		L.player.sprite = "player/punch"
-		L.player.sprite_t = 0.05
-		L.player.sprite_start = L.time()
+		if L.player.on_ground then
+			player_anime("player/punch_from_idle", 0.05)
+		else
+			player_anime("player/punch_from_air", 0.05)
+		end
 		L.player.pr, L.player.pl, L.player.pt = 0, 0, 0
 	end
 end
 
+local jump_speed = -25
+
 local function player_state_handler()
 	if L.player.is_dodging() and L.sprite_finished(L.player) then
-		L.player.sprite = "player/idle"
+		if L.player.on_ground then
+			L.player.sprite = "player/idle"
+		else
+			L.player.sprite = "player/in_air"
+		end
 	elseif L.player.is_punching() and L.sprite_finished(L.player) then
-		L.player.sprite = "player/idle"
+		if L.player.on_ground then
+			L.player.sprite = "player/idle"
+		else
+			L.player.sprite = "player/in_air"
+		end
+	elseif L.player.is_jump_from_idle() and L.sprite_finished(L.player) then
+		L.print("jumpfromidle")
+		L.player.jumped_midair = false
+		L.player.vel_y = jump_speed * movement_const
+		L.player.sprite = "player/in_air"
 	end
 end
+
 local function player_movement()
-	if not L.player.on_ground and not L.player.is_inair() then
+	if not L.player.on_ground and not L.player.is_inair() and not L.player.is_dodging() and not L.player.is_punching() then
 		L.player.sprite = "player/in_air"
 		L.player.pr, L.player.pl, L.player.pt = 0, 0, 0
 	end
 
 	local player_speed = L.player.speed
-	local jump_speed = -25
+
 
 	if L.key_down("space") then
-		if L.player.on_ground then
-			L.player.vel_y = jump_speed * movement_const
-			L.player.jumped_midair = false
+		if L.player.on_ground and not L.player.is_jump_from_idle() then
+			player_anime("player/jump_from_idle", 0.03)
 			-- reset this
 			L.key_pressed("space")
 		elseif L.key_pressed("space") and not L.player.jumped_midair then
@@ -109,24 +136,25 @@ local function player_movement()
 		end
 	end
 
-	if L.player.is_dodging() or not L.player.on_ground then
-		return
-	end
-	
+
 	if L.key_down("d") then
 		L.player.vel_x = player_speed * movement_const
-		L.player.sprite_t = 0.1
-		L.player.sx = 1
-		L.player.sprite = "player/runnin"
-		L.player.pr, L.player.pl, L.player.pt = 0, 0, 0
+		if not L.player.is_jump_from_idle() and L.player.on_ground and not L.player.is_dodging() then
+			L.player.sprite_t = 0.1
+			L.player.sx = 1
+			L.player.sprite = "player/runnin"
+			L.player.pr, L.player.pl, L.player.pt = 0, 0, 0
+		end
 	elseif L.key_down("a") then
 		L.player.vel_x = -player_speed * movement_const
-		L.player.sprite_t = 0.1
-		L.player.sx = -1
-		L.player.sprite = "player/runnin"
-		L.player.pr, L.player.pl, L.player.pt = 0, 0, 0
+		if not L.player.is_jump_from_idle() and L.player.on_ground and not L.player.is_dodging() then
+			L.player.sprite_t = 0.1
+			L.player.sx = -1
+			L.player.sprite = "player/runnin"
+			L.player.pr, L.player.pl, L.player.pt = 0, 0, 0
+		end
 	else
-		if not L.player.is_punching() then
+		if not L.player.is_punching() and not L.player.is_dodging() and not L.player.is_jump_from_idle() then
 			L.player.sprite = "player/idle"
 			L.player.sprite_t = 0.1
 			L.player.pr = (L.player.sx == -1) and -30 or -3
@@ -152,11 +180,9 @@ function L.base_player_loop()
 	player_movement()
 end
 
-
-
 function L.base_dialogue_loop()
 	L.play_dialogue()
-	for k,obj in ipairs(L.active_level.level.np_objects) do
+	for k, obj in ipairs(L.active_level.level.np_objects) do
 		if obj.tag == "text" then
 			if obj.is_dead_at < L.time() then
 				L.active_level.level.np_objects[k] = nil
