68383f2ee435d2ace497e7c2d0015c269f0d559e
vizdun <vizdun@proton.me>
2026-04-25 20:54:07 +0200

    BULLSHIT

--- draclove-love/src/level3.lua
+++ draclove-love/src/level3.lua
@@ -25,7 +25,17 @@ local function generate_pipes()
 end
 
 local origin_x = 0
-local origin_y = -720/2
+local origin_y = -720 / 2
+
+---@enum
+local states = {
+    moving_around = 1,
+    spilling_prep = 2,
+    spilling = 3,
+    spilling_hard = 5,
+    spilling_finishing = 6,
+    spilling_recover = 4,
+}
 
 function lvl3.setup()
     L.boss = {
@@ -35,7 +45,7 @@ function lvl3.setup()
         -- sx=2,
         s = 4,
         r = 0,
-        -- state = door_states.staying,
+        state = states.moving_around,
         -- state_begin = L.time(),
         -- next_state = 1,
         hp = 3,
@@ -48,9 +58,11 @@ function lvl3.setup()
     Player.setup()
     L.pipes = {}
     L.water_projs = {}
+    L.spill = nil
+    L.water_level = nil
 end
 
-local toilet_speed = 50
+local toilet_speed = 500
 local proj_speed = 200
 local pellet_count = 4
 local pellet_angle = 30
@@ -62,33 +74,103 @@ local function shoot_water()
     for i = 0, pellet_count do
         local cr = r + original_angle + i * each_angle
         local vx, vy = L.angle_vec(cr)
-        L.water_projs[L.uid()] = {x=L.boss.x, y=L.boss.y, vel_x = vx * proj_speed, vel_y = vy * proj_speed, born = L.time()}
+        L.water_projs[L.uid()] = { x = L.boss.x, y = L.boss.y, vel_x = vx * proj_speed, vel_y = vy * proj_speed, born = L
+        .time() }
     end
 end
 
+local prep_rot_speed = 50
+local flow_speed = 10
+local magic_y = 540 / 2
+
 local function do_toilet()
-    if not L.boss.target then
-        L.boss.target = { x = L.boss.x, y = L.boss.y }
-        while L.dist(L.boss, L.boss.target) < 200 or math.abs(L.boss.y - L.boss.target.y) < 100 or math.abs(L.boss.x - L.boss.target.x) < 100 do
-            L.boss.target = { x = math.random() * 550 * 2 - 550, y = math.random() * 160 * 2 - 160 }
+    if L.boss.state == states.moving_around then
+        if not L.boss.target then
+            L.boss.target = { x = L.boss.x, y = L.boss.y }
+            while L.dist(L.boss, L.boss.target) < 200 or math.abs(L.boss.y - L.boss.target.y) < 100 or math.abs(L.boss.x - L.boss.target.x) < 100 do
+                L.boss.target = { x = math.random() * 550 * 2 - 550, y = math.random() * 160 * 2 - 160 }
+            end
+            local vx, vy = L.vec_to(L.boss.target, L.boss)
+            L.boss.vel_x = vx * toilet_speed
+            L.boss.vel_y = vy * toilet_speed
+            if L.boss.vel_x < 0 then
+                L.boss.sx = -1
+            else
+                L.boss.sx = 1
+            end
         end
-        local vx, vy = L.vec_to(L.boss.target, L.boss)
-        L.boss.vel_x = vx * toilet_speed
-        L.boss.vel_y = vy * toilet_speed
-        if L.boss.vel_x < 0 then
-            L.boss.sx = -1
-        else
-            L.boss.sx = 1
+
+        if L.dist(L.boss, L.boss.target) < 10 then
+            L.boss.vel_x, L.boss.vel_y = 0, 0
+            L.boss.target = nil
+            L.boss.state = states.spilling_prep
+            shoot_water()
+        end
+
+        L.move_vel(L.boss)
+    end
+
+    local r_mod = L.boss.sx > 0 and 1 or -1
+    if L.boss.state == states.spilling_prep then
+        L.boss.r = (L.boss.r or 0) + prep_rot_speed * r_mod * L.dt
+        if r_mod == 1 and L.boss.r >= 180 or r_mod == -1 and L.boss.r <= -180 then
+            L.boss.r = 180 * r_mod
+            L.boss.state = states.spilling
+            L.boss.state_start = L.time()
         end
     end
 
-    if L.dist(L.boss, L.boss.target) < 10 then
-        L.boss.vel_x, L.boss.vel_y = 0, 0
-        L.boss.target = nil
-        shoot_water()
+    local xoff, yoff = L.boss.sx > 0 and -40 or 40,-5
+
+    if L.boss.state == states.spilling then
+        if not L.spill then
+            L.spill = {}
+        end
+        local d = L.time() - L.boss.state_start
+        L.spill.x = L.boss.x + xoff
+        L.spill.y = L.boss.y + 32 * d * flow_speed / 2 + yoff
+        L.spill.sy = d * flow_speed
+        if L.spill.y >= (L.boss.y + magic_y) / 2 then
+            -- L.spill.y = (L.boss.y + magic_y) / 2
+            -- L.spill.sy = (L.boss.y + magic_y) / 13
+            L.boss.state = states.spilling_hard
+            L.boss.state_start = L.time()
+        end
     end
 
-    L.move_vel(L.boss)
+    if L.boss.state == states.spilling_hard then
+        if not L.water_level then
+            L.water_level = {x=0,y=L.height/2, sx=40, sy=8}
+        end
+        local d = L.time() - L.boss.state_start
+        L.water_level.sy = d * flow_speed
+        if L.water_level.sy > 13 then
+            L.water_level.sy = 13
+             L.boss.state = states.spilling_finishing
+            L.boss.state_start = L.time()
+        end
+    end
+
+    if L.boss.state == states.spilling_finishing then
+        local d = L.time() - L.boss.state_start
+        L.water_level.sy = 13 - d * flow_speed * 0.5
+        L.spill.y = (L.boss.y + magic_y) / 2 + 32 * d * flow_speed / 2 + yoff
+        L.spill.sy = math.max((magic_y - L.boss.y) / 32 - d * flow_speed, 0)
+        if L.water_level.sy < 0 then
+            L.water_level.sy = 0
+            L.boss.state = states.spilling_recover
+            L.boss.state_start = L.time()
+        end
+    end
+
+    if L.boss.state == states.spilling_recover then
+        L.boss.r = (L.boss.r or 0) - prep_rot_speed * r_mod * L.dt
+        if (r_mod == 1 and L.boss.r <= 0) or (r_mod == -1 and L.boss.r >= 0) then
+            L.boss.r = 0
+            L.boss.state = states.moving_around
+            L.boss.state_start = nil
+        end
+    end
 end
 
 function lvl3.loop(dt)
@@ -111,9 +193,15 @@ function lvl3.loop(dt)
     end
 
     local pipes = {}
-    table.insert(pipes, { c=L.hit_time and "#FF5050", sprite = "idibiks/pipe", r=90, x = (origin_x + L.boss.x) / 2, y = L.boss.y, sy = math.abs(origin_x - L.boss.x) / 64, sx=L.boss.x > 0 and -1 or 1 })
-    table.insert(pipes, { c=L.hit_time and "#FF5050", sprite = "idibiks/pipe", x = origin_x, y = (origin_y + L.boss.y) / 2, sy = math.abs(origin_y - L.boss.y) / 64 })
-    table.insert(pipes, { c=L.hit_time and "#FF5050", sprite = L.boss.x > 0 and "idibiks/joint_right" or "idibiks/joint_left", x = origin_x, y = L.boss.y })
+    table.insert(pipes,
+        { c = L.hit_time and "#FF5050", sprite = "idibiks/pipe", r = 90, x = (origin_x + L.boss.x) / 2, y = L.boss.y, sy =
+        math.abs(origin_x - L.boss.x) / 64, sx = L.boss.x > 0 and -1 or 1 })
+    table.insert(pipes,
+        { c = L.hit_time and "#FF5050", sprite = "idibiks/pipe", x = origin_x, y = (origin_y + L.boss.y) / 2, sy = math
+        .abs(origin_y - L.boss.y) / 64 })
+    table.insert(pipes,
+        { c = L.hit_time and "#FF5050", sprite = L.boss.x > 0 and "idibiks/joint_right" or "idibiks/joint_left", x =
+        origin_x, y = L.boss.y })
 
     if L.player.is_punching() then
         for _i, pipe in ipairs(pipes) do
@@ -125,13 +213,13 @@ function lvl3.loop(dt)
         end
     end
 
-    L.draw({sprite="scenes/3", s=6.66})
+    L.draw({ sprite = "scenes/3", s = 6.66 })
 
     for _i, pipe in ipairs(pipes) do
         L.draw(pipe)
     end
 
-    L.draw(L.patch(L.boss, {sprite="idibiks/attack", sy=1}))
+    L.draw(L.patch(L.boss, { sprite = "idibiks/attack", sy = 1 }))
 
     for _, proj in pairs(L.water_projs) do
         L.draw(proj)
@@ -139,6 +227,14 @@ function lvl3.loop(dt)
 
     L.draw(L.patch(L.player, { debug = true }))
     L.draw(L.player)
+
+    if L.spill then
+        L.draw(L.spill)
+    end
+    if L.water_level then
+        L.draw(L.water_level)
+    end
+
     -- L.draw(ground_bot)
     -- L.draw(ground_top)
     L.draw_hud()
