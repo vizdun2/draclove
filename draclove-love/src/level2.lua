e381ab3d2620d96f75cfad7ef99a1d09a04d0d73
vizdun <vizdun@proton.me>
2026-04-26 15:25:15 +0200

    more bs bs

--- draclove-love/src/level2.lua
+++ draclove-love/src/level2.lua
@@ -314,16 +314,18 @@ function lvl2.loop(dt)
         local collide, fromAbove, _ = gravity.check_collide(L.player, L.boss)
         if collide then
             if (L.boss.state == "grounded" or not L.pasttime(L.boss.last_grounded + 0.5)) and fromAbove then
-                L.player.vel_y = -2000
-                L.boss.hp = L.boss.hp - 1
-                L.boss.sprite = "door/door_damaga"
-                L.boss.sprite_t = 0.1
-                L.boss.sprite_start = L.time()
-                L.print("Boss Hit! HP:", L.boss.hp)
+                if L.boss.sprite ~= "door/door_damaga" then
+                    L.player.vel_y = -2000
+                    L.boss.hp = L.boss.hp - 1
+                    L.boss.sprite = "door/door_damaga"
+                    L.boss.sprite_t = 0.1
+                    L.boss.sprite_start = L.time()
+                    L.print("Boss Hit! HP:", L.boss.hp)
 
 
-                L.boss.timeHitGround = 0
-                L.play("audio/door_slam", 0.4)
+                    L.boss.timeHitGround = 0
+                    L.play("audio/door_cracked", 0.6)
+                end
             elseif L.boss.state ~= "grounded" then
                 L.player.take_damage()
             end
