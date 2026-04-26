aa6f25c4379479e6c056216f07eb0471efe552b7
vizdun <vizdun@proton.me>
2026-04-26 12:42:09 +0200

    blah

--- buildwasm.sh
+++ buildwasm.sh
@@ -2,7 +2,7 @@
 
 mkdir -p out
 rm -rf out/web out/draclove_web.zip
-npx love.js -c -t draclove draclove-love out/web
+npx love.js --memory 41943040 -c -t draclove draclove-love out/web
 rm -r out/web/theme
 cp misc/index.html out/web
 # miniserve --index index.html out/web
